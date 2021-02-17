30ca5b9b0f83e9645a09517c4924592c01d32571
Levi Daymon <levithehandyman@outlook.com>
2021-02-17 15:36:39 -0500

    added const MEDIA_URL to se as template literal

--- src/index.js
+++ src/index.js
@@ -2,48 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 
-/**
- * Hey, your app is pretty nice! The code is D.R.Y., and work well, but you could give
- * some attention to your code compositions habits. Just a few overall notes:
- *
- * You can have code, but if it's messy, it can be hard to understand, and hard to read.
- * In clean code, consistency is key. Some examples:
- * 
- * 1. In your code some lines end with a `;` while others do not. Either is correct,
- * but both can be construed as "messy." (Habitually ending lines with a semicolon can
- * protect from some weird bugs, so I usually use them). For more on semicolons in JS
- * see: <https://medium.com/better-programming/the-use-of-the-javascript-semicolon-843fd5e94d0e#:~:text=Semicolons%20are%20an%20essential%20part,other%20parts%20of%20the%20code.>
- *
- * 2. Some variables, class methods, and functions have a single line after them, some do
- * not, and some have more than one line after them. For example in the `App` class,
- * there is 1 line after the `constructor`, 2 lines after the `playSound` function.
- * Usually a single line after each class member makes for better readability. Kind of a
- * "visual rhythm" if you will.
- *
- * 3. Some strings or React Element attribute values have `'` (single quotes) surrounding
- * them, some have `"` (double-quotes). Either is correct, but like #1 and #2 above,
- * but having a mix of both is inconsistent.
- *
- * Why all the piddly little nitpicks? When you write code, you are always writing it
- * for someone else, even if no one else besides you will ever touch it. The other
- * person is usually your future self. The more consistent and readable your code is,
- * the easier it will be to maintain or extend later!
- *
- * The above notes are comments related to things that are fairly subjective, as in
- * developers will debate about which thing is "better," but the standard itself is
- * less important than just picking one and sticking with it.
- *
- * Tip: There is a library called Prettier I like to use which gets installed as part of
- * the developer tools on your local machine, and just fixes all these little things
- * automatically, which is nice.
- */
-
-// In some JS best-practice guides, you might find a preference for naming variables with
-// underscore-delimited, upper-case names (like so: `UPPER_CASE`), whenever the variable
-// exists at the global scope, like this one does here.
-//
-// I'd suggest naming this RADIO_SOUNDS. This has a readability benefit of the reader
-// instantly knowing that this is a global variable simply by looking at it.
+const MEDIA_URL = 'https://res.cloudinary.com/dnv60ey6k/video/upload/fitter/';
 const RADIO_SOUNDS = [
   /**
    * Your `src` property in the objects below seem to share some common things.
@@ -73,7 +32,7 @@ const RADIO_SOUNDS = [
     key: 'Q',
     audioID: 'fitter',
     // Seems like this one isn't up in cloudinary, it's a 404
-    src: 'https://res.cloudinary.com/dnv60ey6k/video/upload/v1613076443/fitter_riupy7.wav'
+    src: `${MEDIA_URL}/fitter_riupy7.wav`
   },
  
   {
@@ -194,7 +153,7 @@ class App extends React.Component{
   // variable name.
   playSound = (val, audioID) => {
     const audio = document.getElementById(val);
-    
+
     audio.currentTime = 0;
     audio.play();
    
